a24de22d8a5febab3af8e3ed241e5fe32b77b2ce
Jinwook94 <kimjinwook94@gmail.com>
2025-04-01 15:16:15 +0900

    [client-web] 라우터 추가 (react-router-dom)

--- client-web/src/App.tsx
+++ client-web/src/App.tsx
@@ -1,5 +1,12 @@
-function App() {
-  return <></>;
-}
+import { Route, Routes } from "react-router-dom";
+import PATH from "@/constants/path.ts";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path={PATH.HOME} element={<></>} />
+    </Routes>
+  );
+};
 
 export default App;
