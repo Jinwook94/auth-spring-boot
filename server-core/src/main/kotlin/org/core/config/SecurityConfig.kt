f6a02f2e5336272c24edfcb85f2b682e7cecd413
Jinwook94 <kimjinwook94@gmail.com>
2025-04-07 19:14:07 +0900

    [server-core] 필터 발생 예외의 일관된 처리를 위해 FilterExceptionHandler 구현

--- server-core/src/main/kotlin/org/core/config/SecurityConfig.kt
+++ server-core/src/main/kotlin/org/core/config/SecurityConfig.kt
@@ -1,5 +1,6 @@
 package org.core.config
 
+import org.core.exception.FilterExceptionHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
@@ -7,11 +8,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.context.SecurityContextHolderFilter
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
-class SecurityConfig {
+class SecurityConfig(
+	private val filterExceptionHandler: FilterExceptionHandler
+) {
 
 	@Bean
 	fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
@@ -26,6 +30,8 @@ class SecurityConfig {
 					.requestMatchers("/actuator/**").permitAll()
 					.anyRequest().authenticated()
 			}
+			// 예외 처리 필터를 Spring Security 필터 체인의 가장 앞에 배치
+			.addFilterBefore(filterExceptionHandler, SecurityContextHolderFilter::class.java)
 			.build()
 	}
 
